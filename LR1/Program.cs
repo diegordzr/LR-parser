9b6815e90ae5c375c530bba844c18d1c667a4e11
Diego Rodríguez <diego.rx2@gmail.com>
2015-01-19 22:28:33 -0600

    Se agrego el metodo GetBeta(), posteriormente modificar el metodo GoTo para recorrer punto

--- LR1/Program.cs
+++ LR1/Program.cs
@@ -13,6 +13,14 @@ namespace LR1
 				"S -> C C",
 				"C -> c C | d"
 			};
+
+			gramarG = new []{ 
+				"S' -> S",
+				"S -> id | V igual E",
+				"V -> id",
+				"E -> V | num"
+			};
+
 			Items (gramarG);
 		}
 
@@ -22,10 +30,11 @@ namespace LR1
 			for (var i = 0; i < I.Count; i++) {
 				var elements = GetElements(I[i]);
 				var eB = elements [2];
-				var beta = elements [3];
+				var beta = elements[3];
 				var a = elements [4];
+				var aBeta = new []{ beta, a };
                 foreach (var gama in GetGama(eB, gramarGp)){
-                    foreach (var b in First(new[] { beta, a }, gramarGp)){
+					foreach (var b in First(aBeta, gramarGp)){
 						var prod = eB + " -> ." + gama + ", " +  b;
 						I.Add(prod);
 					}
@@ -41,7 +50,7 @@ namespace LR1
 				var eA = elements[0];
 				var alpha = elements [1];
 			    var eX = elements[2];
-				var beta = elements [3];
+				var beta = string.Join(" ", GetBeta(prod));
 				var a = elements [4];
 			    if (!string.IsNullOrEmpty(x) && eX == x)
 			    {
@@ -70,6 +79,7 @@ namespace LR1
 			}
 		}
 			
+
 		public static string[] First(string[] elements, string[] gramar){
 			var firsts = new List<string> ();
 			foreach (var element in elements) {
@@ -143,6 +153,25 @@ namespace LR1
 			return listTerminals.ToArray();
 		}
 
+		public static string[] GetBeta(string production){
+			const string patternBbeta = @"\.[A-Za-z ]+";
+			var matchesBbeta = Regex.Matches (production, patternBbeta).Cast<Match>().Select(m=>m.Value);
+
+			var elementBbeta = matchesBbeta.FirstOrDefault();
+			string elementB = null; 
+			string beta = null;
+			if(elementBbeta != null){
+				var elements = elementBbeta.Split(' ');
+				if (elements.Count () >= 2) {
+					var betaElements = elements.ToList();
+					betaElements.RemoveAt(0);
+					return betaElements.ToArray();
+				}
+			}
+			return new string[]{};
+		}
+
+
 		public static bool IsTerminal(string element)
 		{
 		    return element.All(t => !char.IsUpper(t));
@@ -167,10 +196,12 @@ namespace LR1
 			string beta = null;
 			if(elementBbeta != null){
 				var elements = elementBbeta.Split(' ');
-				if(elements.Any())
-					elementB = elementBbeta.Split(' ')[0];
-				if (elements.Count() == 2)
-					beta = elementBbeta.Split (' ') [1];
+				if (elements.Any ()) {
+					elementB = elements [0];
+				}
+				if (elements.Count () >= 2) {
+					beta = elements[1];
+				}
 			}
 
             var a = Regex.Matches(production, aPattern).Cast<Match>().Select(m => m.Value).FirstOrDefault();
