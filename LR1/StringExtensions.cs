e45344c48ce5a9945fec2e91783b8ce2ccab4a14
Diego Rodríguez <diego.rx2@gmail.com>
2015-02-17 23:45:34 -0600

    Se genera la tabla canónica LR
    tabla LR terminada falta impresión en pantalla y se incluyeron algunos
    métodos de Edgar Aron

---
+++ LR1/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LR1
+{
+	public static class StringExtensions
+	{
+		public static string ToMeta(this string value)
+		{
+			var regexOperators = new[] { "-", "+", ".", "(", ")", "|", "?", "*" };
+			foreach (var regexOp in regexOperators){
+				value = value.Replace(regexOp, @"\" + regexOp);
+			}
+			return value;
+		}
+	}
+}
+
--- LR1/Tools.cs
+++ LR1/Tools.cs
@@ -1,16 +1,18 @@
-﻿using System.Linq;
-using System.Text.RegularExpressions;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace LR1
 {
 
 	public static class Tools
 	{
-		private const string _terminalPattern = "[a-z]+";
-		private const string _noTerminalPattern = "[A-Z]+";
-		private const string _producePattern = "->";
-
+		private const string _terminalPattern = "`[^`]+`";
+		private const string _noTerminalPattern = @"[\w]+'?";
+		private const string _produceElement = ":";
+		private const string _orElement = "|";
 
 		public static string[] First(string[] elements, string[] gramar){
 			var firsts = new List<string> ();
@@ -31,7 +33,7 @@ namespace LR1
 				firsts.Add (element.Trim());
 			} else {
 				foreach (var production in gramar) {
-					var elementA = Regex.Split (production, _producePattern).First ().Trim ();
+					var elementA = Regex.Split (production, _produceElement).First ().Trim ();
 					if (element == elementA) {
 						var prodFirsts = First (production);
 						firsts.AddRange (prodFirsts);
@@ -42,26 +44,23 @@ namespace LR1
 		}
 
 		public static string[] First(string production){
-			string patternTerminal1 = string.Format("{0}( )*{1}", _producePattern, _terminalPattern);
-			string patternTerminal2 = string.Format("\\|( )*{0}", _terminalPattern);
+			string patternTerminal = string.Format ("({0} *|{1} *){2}", _produceElement.ToMeta(), _orElement.ToMeta(), _terminalPattern);
+			var terminals = Regex.Matches(production, patternTerminal).Cast<Match>().Select(m=>m.Value);
+					
+			var terminalsList = terminals.Select(t => 
+				t.Replace(_produceElement, string.Empty).Replace(_orElement, string.Empty)
+				.Trim()).ToList();
 
-			var terminal = Regex.Matches (production, patternTerminal1).Cast<Match>().Select(m => m.Value);
-			var terminals = Regex.Matches(production, patternTerminal2).Cast<Match>().Select(m=>m.Value);
+			if (!terminalsList.Any ())
+				terminalsList.Add("$");
 
-			terminal = terminal.Select(t => t.Replace(_producePattern, string.Empty).Trim());
-			terminals = terminals.Select(t => t.Replace("|", string.Empty).Trim());
-
-			var listTerminals = terminal.Union(terminals).ToList();
-			if (!listTerminals.Any ())
-				listTerminals.Add ("$");
-
-			return listTerminals.ToArray();
+			return terminalsList.ToArray();
 		}
 
 		public static string[] GetGama(string elemA, string[] gramar){
 			var gamas = new List<string> ();
 			foreach(var production in gramar){
-				var elementA = Regex.Split(production, _producePattern).First().Trim();
+				var elementA = Regex.Split(production, _produceElement).First().Trim();
 				if (elementA == elemA) {
 					var prodGamas = GetGama (production).ToList();
 					gamas = gamas.Union(prodGamas).ToList();
@@ -72,25 +71,17 @@ namespace LR1
 
 
 		public static string[] GetGama(string production){
-			string patternTerminal1 = string.Format ("{0}( )*({1}|{2}| )+", _producePattern, _terminalPattern, _noTerminalPattern);
-			string patternTerminal2 = string.Format ("\\|( )*({0}|{1}| )+", _terminalPattern, _noTerminalPattern);
-
-			var terminal = Regex.Matches (production, patternTerminal1).Cast<Match>().Select(m => m.Value);
-			var terminals = Regex.Matches(production, patternTerminal2).Cast<Match>().Select(m=>m.Value);
-
-			terminal = terminal.Select(t => t.Replace(_producePattern, string.Empty).Trim());
-			terminals = terminals.Select(t => t.Replace("|", string.Empty).Trim());
-
-			var listTerminals = terminal.Union(terminals).ToList();
-			return listTerminals.ToArray();
+			string gamaPattern = string.Format ("[^{0}^{1}]+", _orElement.ToMeta(), _produceElement.ToMeta());
+			var gamas = Regex.Matches (production, gamaPattern).Cast<Match>().Select(m => m.Value.Trim()).ToList();
+			gamas.RemoveAt (0);
+			return gamas.ToArray();
 		}
 
 		public static string[] GetBeta(string production){
 			string patternBbeta = string.Format ("\\.({0}|{1}| )+", _terminalPattern, _noTerminalPattern);
 			var matchesBbeta = Regex.Matches (production, patternBbeta).Cast<Match>().Select(m=>m.Value);
 			var elementBbeta = matchesBbeta.FirstOrDefault();
-			//			string elementB = null; 
-			//			string beta = null;
+
 			if(elementBbeta != null){
 				var elements = elementBbeta.Split(' ');
 				if (elements.Count () >= 2) {
@@ -192,19 +183,19 @@ namespace LR1
 		}
 
 
-		private static bool IsPointOfEnd(string production){
+		public static bool IsPointAtEnd(string production){
 			if (production.Contains (".,"))
 				return true;
 			return false;
 		}
 
 		public static string MakeProduction(string elementA, string alpha, string x, string beta, string a){
-			var prod = elementA + " " + _producePattern; 
-			if (alpha != null)
+			var prod = elementA + " " + _produceElement; 
+			if (!string.IsNullOrEmpty(alpha))
 				prod += " " + alpha;
 			if (x != null)
 			{
-				prod += " " + x;
+				prod = prod + " " + x;
 			}
 			prod += ".";
 			if (beta != null)
@@ -215,7 +206,7 @@ namespace LR1
 		}
 
 		public static string MakeProduction(string eB, string gama, string b){
-			return string.Format("{0} -> .{1}, {2}", eB, gama, b);
+			return string.Format("{0} {1} .{2}, {3}", eB, _produceElement, gama, b);
 		}
 
 		public static string[] GetTerminals(string[] gramarGp){
@@ -235,6 +226,213 @@ namespace LR1
 				totalTerminals = totalTerminals.Union(terminals).ToList();
 			}
 			return totalTerminals.ToArray();
+		}
+
+		public static string productionPattern(){
+			string ret ="";
+			ret = string.Format("{0} *{1}({0}|{2}| )+({3}({0}|{2}| )+)+", 
+				_noTerminalPattern, _produceElement.ToMeta(), _terminalPattern, _orElement.ToMeta());   
+
+			return ret;
+		}
+		public static string[] GetTokens(string stringInput)
+		{          
+
+			string pattern = string.Format("{0}|{1}|{2}|{3}", _noTerminalPattern, _terminalPattern, _orElement.ToMeta(), _produceElement);
+			var elements = Regex.Matches(stringInput, pattern).Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+
+			return elements;
+		}
+
+		public static string[] GetProductions(string gramar)
+		{
+			var production = new List<string>();
+			var array = gramar.Split('\n');
+			foreach (var prod in array)
+			{               
+				string pattern = string.Format("{0}|{1}|{2}|{3}", _noTerminalPattern, _terminalPattern, _orElement.ToMeta(), _produceElement.ToMeta());
+				var elements = Regex.Matches(prod, pattern).Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+				var prods = GetSubProduction(elements);
+				production.AddRange(prods);
+			}            
+			return production.ToArray<string>();
+		}
+
+		public static string RetLastElement(string production)
+		{
+			string pattern = string.Format("{0}|{1}|{2}|\\$", _noTerminalPattern, _terminalPattern, ",");
+			var elements = Regex.Matches(production,pattern).Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+			return elements.Last().Trim() ;            
+		}
+
+		public static string Normalization(string[] dato)
+		{
+			string ret = "";            
+
+			foreach (var linea in dato)
+			{
+				string patern = string.Format("{0}|{1}|{2}|{3}", _noTerminalPattern, _terminalPattern, _orElement.ToMeta(), _produceElement.ToMeta());
+				var elements = Regex.Matches(linea,patern).Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+				foreach (var elemnt in elements)                 
+					ret = ret + elemnt + " ";                                 
+				ret = ret.Trim();
+				ret = ret + "\n";
+			}
+			return ret;
+		}
+
+		public static string Increases(string gramar)
+		{
+			string aum = "";
+
+			var produ = Regex.Match(gramar, _noTerminalPattern);
+			var prodfirst = produ.ToString().Replace(">", "'>");
+
+			aum = prodfirst + " " + _produceElement + " " + produ;
+			//Queda en Formato <S'> -> <S>
+			return aum;
+		}
+
+		public static string[] GetSubProduction(string[] elements)
+		{
+			if (elements == null || !elements.Any())
+				return elements;
+
+			List<string> retArrray = new List<string>();
+			var r = new Regex(_noTerminalPattern);
+
+
+			if (!r.IsMatch(elements[0]))
+			{
+				throw new Exception("Error DE Inicio en Produccion" + elements[0]);
+			}
+
+			var first = elements[0];
+			var second = elements[1];
+			var production = "";
+			int inc = 0;
+			foreach(var element in elements)
+			{
+				if (element.Contains(_orElement))
+				{
+					retArrray.Add(production);
+					production = "";
+					inc++;
+				}
+				else
+				{
+					if (inc == 0)  //si no a entrado ninguna vez
+						production = production + element +" ";
+					else
+					{                      //si ya entro una vez                      
+						production = first + " " + second + " " + element +" ";
+						inc = 0;
+					}   
+				}                 
+			}
+			retArrray.Add(production);
+			return retArrray.ToArray();
+		}
+
+		public static int GetIndexX(string cadena, string[] tokenSearch)
+		{
+			for(int i=0; i < tokenSearch.Count(); i++)
+			{
+				if (tokenSearch[i] == cadena)
+					return i;
+			}
+			return -1;
+		}
+
+		public static int GetIndexY(string cadena)
+		{            
+			string pattern = string.Format("{0}", "[0-9]+");
+			var elements = Regex.Matches(cadena, pattern).Cast<Match>().Select(m => m.Value.Trim()).ToArray();            
+
+			return  Convert.ToInt32(elements.Last());
+		}
+
+		public static string GetProduction(string[] gramarGp, int ind)
+		{
+			for (int i = 0; i < gramarGp.Count(); i++)
+				if (i == ind)
+					return gramarGp[i].ToString();
+			return "";
+		}
+
+		public static int GetElementosDelete(string cadena, ref string pater)
+		{
+			int retLength = 0;
+			bool band = false;
+			string pattern = string.Format("{0}|{1}|{2}|{3}", _noTerminalPattern, _terminalPattern, _orElement.ToMeta(), _produceElement.ToMeta());
+			var elements = Regex.Matches(cadena, pattern).Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+
+			foreach (var elemen in elements)
+			{
+				if (band)
+					retLength++;
+				if (elemen == "->")                
+					band = true;
+				if (!band)
+					pater = elemen;
+			}
+			return retLength;
+		}
+
+		public static string GetAlpha(string production)
+		{
+			string retElements = "";
+			bool band = false;
+
+			string pattern = string.Format("{0}|{1}|{2}|{3}", _noTerminalPattern, _terminalPattern, _produceElement.ToMeta(), ".");
+			var elements = Regex.Matches(production, pattern).Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+
+			foreach (var pat in elements)
+			{
+				if (band)
+				{
+					if (pat == ".")                    
+						return retElements.Trim();
+					if(pat == "")
+						retElements = retElements + " ";
+					retElements = retElements + pat;
+				}
+				if (pat == "->")
+					band = true;
+			}
+			return retElements.Trim();
+		}
+
+		public static bool VerificateTokens(string[] tokens, string cadena)
+		{
+			bool band = true;        
+			if (cadena != "")
+			{
+				string pattern = string.Format("{0}|{1}|{2}|{3}", 
+					_noTerminalPattern, _terminalPattern, _orElement.ToMeta(), _produceElement.ToMeta());
+				var elements = Regex.Matches(cadena, pattern).Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+
+				foreach (var element in elements)
+				{
+					bool bandVer = false;
+					for (int i = 0; i < tokens.Count(); i++)
+					{                        
+						if (tokens[i] == element)
+							bandVer = true;
+					}
+					if (bandVer == false)
+						return false;
+				}
+			}
+			return band;
+		}
+
+		public static string FirstProduction(string cadena)
+		{
+			cadena = cadena + ", $";
+			int posfle = cadena.IndexOf(_produceElement);
+			cadena = cadena.Remove(posfle + 2, 1).Insert(posfle + 2, " .");
+			return cadena;
 		}
 	}
 }
